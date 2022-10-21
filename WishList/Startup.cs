b478a6ea728bb285e6f867bd89b4e9d8404c255a
DevHobs <devhobs@gmail.com>
2022-10-20 23:20:53 -0400

    5

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -13,6 +13,8 @@ namespace WishList
 {
     public class Startup
     {
+        private object context;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,22 +23,17 @@ namespace WishList
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.Run(async (context) =>
             if (env.IsDevelopment())
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
-                app.UseDeveloperExceptionPage(); 
-
+                app.UseDeveloperExceptionPage();
+            }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
 
-                app.UseRouting();
-                app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+            app.UseMvcWithDefaultRoute();
         }
     }
-}
